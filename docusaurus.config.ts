2647fd8696c8614ea3e3e40d54fcceb2c41f678e
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-09-07 17:46:54 +0200

    feat: testing out github workflow for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: "https://abianche.github.io/", // Your website URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/skroll-website",
+  baseUrl: "/skroll-website/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
